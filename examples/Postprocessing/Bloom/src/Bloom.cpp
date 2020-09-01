789f3b0cfb6da9eed775fb4960b8667869cb5e6e
Robert Ryan <33040161+rtryan98@users.noreply.github.com>
2020-09-02 01:05:58 +0200

    Categorizing

--- examples/Postprocessing/Bloom/src/Bloom.cpp
+++ examples/Postprocessing/Bloom/src/Bloom.cpp
@@ -6,18 +6,19 @@ constexpr char* title = "Bloom";
 
 int main(int argc, char* argv[])
 {
+	// setup the window.
 	Window window{width, height, title};
 	window.preInit(4, 6);
 	window.create();
 	window.postInit();
-
+	// setup the debug callback.
 	OpenGL::initDebugCallback();
-
+	// setup the shaders used.
 	Shader screenQuadShader{};
 	screenQuadShader.addShader("res/shader/quad.vert.glsl", GL_VERTEX_SHADER);
 	screenQuadShader.addShader("res/shader/quad.frag.glsl", GL_FRAGMENT_SHADER);
 	screenQuadShader.compile();
-
+	// setup the VAO's used.
 	GLuint screenQuadVao{};
 	glCreateVertexArrays(1, &screenQuadVao);
 	glEnableVertexArrayAttrib(screenQuadVao, 0);
@@ -26,7 +27,7 @@ int main(int argc, char* argv[])
 	glEnableVertexArrayAttrib(screenQuadVao, 1);
 	glVertexArrayAttribFormat(screenQuadVao, 1, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float));
 	glVertexArrayAttribBinding(screenQuadVao, 1, 0);
-
+	// setup the VBO's used.
 	GLuint screenQuadVbo{};
 	glCreateBuffers(1, &screenQuadVbo);
 	GLfloat screenQuad[16]
@@ -36,12 +37,11 @@ int main(int argc, char* argv[])
 		-1.0f,  1.0f, 0.0f, 1.0f,
 		 1.0f,  1.0f, 1.0f, 1.0f
 	};
-	glNamedBufferStorage(screenQuadVbo, 16 * sizeof(GLfloat), screenQuad, 0);
+	glNamedBufferStorage(screenQuadVbo, 16 * sizeof(GLfloat), screenQuad, 0x0); // This quad is immutable by all means, thus flags is 0.
 	glVertexArrayVertexBuffer(screenQuadVao, 0, screenQuadVbo, 0, 4 * sizeof(GLfloat));
-
+	// loop
 	while (!window.isCloseRequested())
 	{
-
 
 		// draw the screenquad
 		screenQuadShader.bind();
@@ -49,14 +49,14 @@ int main(int argc, char* argv[])
 		glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 		glBindVertexArray(0);
 		screenQuadShader.unbind();
-
+		// swap window buffers
 		window.render();
 		if (window.isKeyPressed(GLFW_KEY_ESCAPE))
 		{
 			window.close();
 		}
 	}
-
+	// cleanup unused memory.
 	glDeleteBuffers(1, &screenQuadVbo);
 	glDeleteVertexArrays(1, &screenQuadVao);
 
